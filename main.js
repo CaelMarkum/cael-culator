eba9bb3bba28b772c4695793a376668d067a93c8
Cael Markum <camfan1212@gmail.com>
2024-10-30 10:22:17 -0500

    making it work
    maybe

--- main.js
+++ main.js
@@ -1,7 +1,39 @@
-const playButton = document.getElementById("audio")
+//numbers
+const oneButton = document.getElementByID("one")
+const twoButton = document.getElementByID("two")
+const threeButton = document.getElementByID("three")
+const fourButton = document.getElementByID("four")
+const fiveButton = document.getElementByID("five")
+const sixButton = document.getElementByID("six")
+const sevenButton = document.getElementByID("seven")
+const eightButton = document.getElementByID("eight")
+const nineButton = document.getElementByID("nine")
+const zeroButton = document.getElementByID("zero")
 
-const sound = new Audio("test.mp3")
+//operations
+const plusButton = document.getElementByID("plus")
+const minusButton = document.getElementByID("minus")
+const divideButton = document.getElementByID("divide")
+const multiplyButton = document.getElementByID("multiply")
 
-playButton.addEventListener( "click", () => {
-    sound.play()
+//special operations
+const negateButton = document.getElementByID("negate")
+const squareButton = document.getElementByID("square")
+const squareRootButton = document.getElementByID("squareRoot")
+const decimalButton = document.getElementByID("decimal")
+
+//resolve
+const equalButton = document.getElementByID("equal")
+const clearButton = document.getElementByID("clear")
+
+const display = document.getElementById("display")
+
+clearButton.addEventListener("click", ()=> {
+    display.value = ""
 })
+
+
+oneButton.addEventListener("click", ()=> {
+    display.value += "1"
+})
+
